fe86dd59921e76b3f68d8740acb4c9dbca6b04c6
dc <mrdotch@hotmail.com>
2019-04-26 16:38:07 +0800

    init projection for setting logfile

---
+++ secpro/src/main/java/com/dco/secpro/SecproApplication.java
@@ -0,0 +1,27 @@
+package com.dco.secpro;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecproApplication {
+	 private final static Logger logger = LoggerFactory.getLogger(SecproApplication.class); 
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SecproApplication.class, args);
+		int i=0;
+		while(true) {
+			
+			/*
+			 * Runnable r=()->logger.info("new thread is running"); new Thread(r).start();
+			 * 
+			 * logger.info("infoMsg"+i); logger.warn("warnMsg"+i);
+			 * logger.debug("debugMsg"+i);
+			 */
+			i++;
+		}
+	}
+
+}
---
+++ secpro/src/test/java/com/dco/secpro/SecproApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dco.secpro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecproApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
